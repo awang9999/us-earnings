3cdb3ae27cee463e2a2f9938d46541b5e6db6a03
Alexander Wang <aw576@cornell.edu>
2020-06-20 13:59:55 -0700

    replaced calendarAPI in scraper with correct/latest version

--- scraper/calendarAPI.py
+++ scraper/calendarAPI.py
@@ -5,10 +5,11 @@ from stringset import StringSet
 BEFORE_OPEN = 0
 AFTER_CLOSE = 1
 UNSPECIFIED = 2
+TAS = 3
 
 
 def convert_date(date):
-    return date.strftime('%Y-%m-%d')
+    return date.st~rftime('%Y-%m-%d')
 
 
 class CalendarAPI:
@@ -38,26 +39,37 @@ class CalendarAPI:
                 sql = "UPDATE calendar SET after_close = %s WHERE date = %s"
                 val = (tickers.to_string(), convert_date(date))
                 self.cur.execute(sql, val)
-            else:
+            elif(time == UNSPECIFIED):
                 tickers.from_string(row[3])
                 tickers.insert(ticker)
                 sql = "UPDATE calendar SET unspecified = %s WHERE date = %s"
                 val = (tickers.to_string(), convert_date(date))
                 self.cur.execute(sql, val)
+            else:
+                tickers.from_string(row[4])
+                tickers.insert(ticker)
+                sql = "UPDATE calendar SET tas = %s WHERE date = %s"
+                val = (tickers.to_string(), convert_date(date))
+                self.cur.execute(sql, val)
         else:
             if (time == BEFORE_OPEN):
-                sql = "INSERT INTO calendar (date, before_open, after_close, unspecified) VALUES (%s, %s, %s, %s)"
-                val = (convert_date(date), ticker, None, None)
+                sql = "INSERT INTO calendar (date, before_open, after_close, unspecified, tas) VALUES (%s, %s, %s, %s, %s)"
+                val = (convert_date(date), ticker, None, None, None)
                 # print("run " + sql % val)
                 self.cur.execute(sql, val)
             elif(time == AFTER_CLOSE):
-                sql = "INSERT INTO calendar (date, before_open, after_close, unspecified) VALUES (%s, %s, %s, %s)"
-                val = (convert_date(date), None, ticker, None)
+                sql = "INSERT INTO calendar (date, before_open, after_close, unspecified, tas) VALUES (%s, %s, %s, %s, %s)"
+                val = (convert_date(date), None, ticker, None, None)
+                # print("run " + sql % val)
+                self.cur.execute(sql, val)
+            elif(time == UNSPECIFIED):
+                sql = "INSERT INTO calendar (date, before_open, after_close, unspecified, tas) VALUES (%s, %s, %s, %s, %s)"
+                val = (convert_date(date), None, None, ticker, None)
                 # print("run " + sql % val)
                 self.cur.execute(sql, val)
             else:
-                sql = "INSERT INTO calendar (date, before_open, after_close, unspecified) VALUES (%s, %s, %s, %s)"
-                val = (convert_date(date), None, None, ticker)
+                sql = "INSERT INTO calendar (date, before_open, after_close, unspecified, tas) VALUES (%s, %s, %s, %s, %s)"
+                val = (convert_date(date), None, None, None, ticker)
                 # print("run " + sql % val)
                 self.cur.execute(sql, val)
 
@@ -80,10 +92,16 @@ class CalendarAPI:
                 sql = "UPDATE calendar SET after_close = %s WHERE date = %s"
                 val = (tickers.to_string(), convert_date(date))
                 self.cur.execute(sql, val)
-            else:
+            elif(time == UNSPECIFIED):
                 tickers.from_string(row[3])
                 tickers.remove(ticker)
                 sql = "UPDATE calendar SET unspecified = %s WHERE date = %s"
+                val = (tickers.to_string(), convert_date(date))
+                self.cur.execute(sql, val)
+            else:
+                tickers.from_string(row[4])
+                tickers.remove(ticker)
+                sql = "UPDATE calendar SET tas = %s WHERE date = %s"
                 val = (tickers.to_string(), convert_date(date))
                 self.cur.execute(sql, val)
 
@@ -100,3 +118,38 @@ class CalendarAPI:
             database="usearnings"
         )
         self.cur = self.con.cursor()
+
+
+# # TESTS
+# capi = CalendarAPI()
+
+# # Alphabetically sorted after insertion test
+# some_day = datetime.date(2020, 6, 16)
+# capi.add_ticker(some_day, 'TSLA', AFTER_CLOSE)
+# capi.add_ticker(some_day, 'AAPL', AFTER_CLOSE)
+# capi.add_ticker(some_day, 'DAL', AFTER_CLOSE)
+# capi.add_ticker(some_day, 'BAC', AFTER_CLOSE)
+# capi.add_ticker(some_day, 'BA', AFTER_CLOSE)
+
+# # before_open and after_close shouldn't affect each other. duplicates across two
+# # columns is allowed.
+# some_day = datetime.date(2020, 6, 17)
+# capi.add_ticker(some_day, 'AAPL', BEFORE_OPEN)
+# capi.add_ticker(some_day, 'AAPL', AFTER_CLOSE)
+# capi.add_ticker(some_day, 'AAPL', UNSPECIFIED)
+# capi.add_ticker(some_day, 'AAPL', AFTER_CLOSE)
+# capi.add_ticker(some_day, 'AAPL', BEFORE_OPEN)
+# capi.add_ticker(some_day, 'AAPL', TAS)
+
+# # Inserting duplicate tickers test
+# some_day = datetime.date(2020, 6, 18)
+# capi.add_ticker(some_day, 'LULU', AFTER_CLOSE)
+# capi.add_ticker(some_day, 'LULU', AFTER_CLOSE)
+# capi.add_ticker(some_day, 'MSFT', AFTER_CLOSE)
+# capi.add_ticker(some_day, 'LULU', AFTER_CLOSE)
+# capi.add_ticker(some_day, 'MSFT', AFTER_CLOSE)
+# capi.remove_ticker(some_day, 'PPL', AFTER_CLOSE)
+
+# capi.remove_date(date=datetime.date(2020, 6, 18))
+# capi.remove_date(date=datetime.date(2020, 6, 17))
+# capi.remove_date(date=datetime.date(2020, 6, 16))
